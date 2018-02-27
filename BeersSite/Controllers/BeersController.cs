f6476029b11d0bc9ace25e8f566051e6305a4664
Santiago <santie88@gmail.com>
2018-02-27 09:34:21 +0100

    Add Beers controller and Index and Details Views

---
+++ BeersSite/Controllers/BeersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeersSite.Controllers
+{
+    public class BeersController : Controller
+    {
+        // GET: Beers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Beer/id
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
